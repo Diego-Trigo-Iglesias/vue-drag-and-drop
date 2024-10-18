b1053cf29ffa068a3c2f9b9405a306e00f5858d9
Diego <diegotrigoiglesias@gmail.com>
2024-10-18 11:21:08 +0200

    aplicacion hecha con Vue 3 para practicac el drag and drop de los elementos

---
+++ src/components/InputNew.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, defineProps } from "vue";
+const props = defineProps(["onNewItem"]);
+const text = ref("");
+const emits = defineEmits(["onNewItem"]);
+
+function handleKeyDown(evt) {
+    if (evt.key === "Enter") {
+        emits("onNewItem", text);
+        text.value = "";
+    }
+}
+</script>
+
+<template>
+    <input v-model="text" @keydown="handleKeyDown" />{{ text }}
+</template>
+
+<style scoped>
+input {
+    margin-bottom: 20px;
+}
+</style>
